569a8a44b45f4a70af41bb0cd709941308518570
rsonaje <sonaje.rohit@gmail.com>
2021-10-26 18:30:38 +0530

    Initial Commit

---
+++ BaseProject/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject
+{
+    public class HelloWorld
+    {
+        public string VersionInfo 
+        {
+            get { return "This is default version"; }
+        }
+
+    }
+} 
